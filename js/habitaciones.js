9d49d4060fc105b3fa291d50b02d6213b13ee805
gvdeveloper <gonzalovono@gmail.com>
2021-09-11 16:25:04 -0300

    Se agrega jquery

---
+++ js/habitaciones.js
@@ -0,0 +1,51 @@
+/* let individual = $("#btn-individual");
+individual.click(mostrar);
+function mostrar(){
+    $("#hab-individual").show();
+} */
+
+
+$(document).ready(()=> {
+    $("#hab-individual").hide();
+    $("#hab-standard").hide();
+    $("#hab-familiar").hide();
+    $("#hab-suite").hide();
+
+    //MOSTRAR HABITACION INDIVIDUAL
+    
+    $("#mostrar-individual").click(function() {
+    $("#hab-individual").fadeIn(800);
+    $("#hab-standard").hide();
+    $("#hab-familiar").hide();
+    $("#hab-suite").hide();
+    });
+    //FIN MOSTRAR HABITACION INDIVIDUAL
+
+    //MOSTRAR HABITACION STANDARD
+    $("#mostrar-standard").click(function() {
+        $("#hab-individual").hide();
+        $("#hab-standard").fadeIn(800);
+        $("#hab-familiar").hide();
+        $("#hab-suite").hide();
+        });
+    //FIN MOSTRAR HABITACION STANDARD
+
+    //MOSTRAR HABITACION FAMILIAR
+    $("#mostrar-familiar").click(function() {
+        $("#hab-individual").hide();
+        $("#hab-standard").hide();
+        $("#hab-familiar").fadeIn(800);
+        $("#hab-suite").hide();
+        });
+    //FIN MOSTRAR HABITACION FAMILIAR
+
+
+    //MOSTRAR HABITACION SUITE
+    $("#mostrar-suite").click(function() {
+        $("#hab-individual").hide();
+        $("#hab-standard").hide();
+        $("#hab-familiar").hide();
+        $("#hab-suite").fadeIn(800);
+        });
+    //FIN MOSTRAR HABITACION SUITE
+});
--- js/registroUsuario.js
+++ js/registroUsuario.js
@@ -1,4 +1,4 @@
-let registrarse = document.getElementById("btn-registrarse");
+/* let registrarse = document.getElementById("btn-registrarse");
     registrarse.addEventListener("click", registrarAlert);
         function registrarAlert(){
             var nombre = document.getElementById("nombreRegistro").value;
@@ -8,3 +8,16 @@ let registrarse = document.getElementById("btn-registrarse");
             
             alert("Bienvenido, usted se ha registrado");
         }
+ */
+
+/* JQUERY */
+
+let registrarse = $("#btn-registrarse");
+registrarse.click(registrarAlert);
+function registrarAlert(){
+            var nombre = document.getElementById("nombreRegistro").value;
+            var email = document.getElementById("emailRegistro").value;
+            localStorage.setItem("nombre",nombre);
+            localStorage.setItem("email", email);           
+            alert("Bienvenido, usted se ha registrado");
+}
